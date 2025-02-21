3f837f9ea72bc4d829bb8c1912c0cad43957c666
aditya-dev <adity119@gmail.com>
2025-02-21 12:31:37 +0530

    useRef-useState-UseEfect-demo

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    setCount(count + 1);
+  }
+
+  const btnRef = useRef();
+
+  //  use effect variation on every render
+  useEffect(() => {
+    console.log("re-render again");
+  });
+
+  const handleColor =() => {
+    btnRef.current.style.backgroundColor = "red";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button ref={btnRef} onClick={handleCount}>Increment</button>
+      <br/>
+      <button onClick={handleColor}>Change Color</button>
+      <br/>
+      <div>
+        Count: {count}
+      </div>
     </div>
   );
 }
